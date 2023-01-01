371c4dc4316b62cca86e9068e805ffc50ba416fe
ohnas <ohnas12@gmail.com>
2023-01-01 16:14:14 +0900

    20. saleRetrieve

--- JS/integrations.js
+++ JS/integrations.js
@@ -5,6 +5,9 @@ const logOutBtn = document.querySelector(".button");
 const integrationForm = document.querySelector(".integration-form");
 const productSelect = integrationForm.querySelector("select");
 const dateInput = integrationForm.querySelector("input");
+const salesTable = document.querySelector(".sales-table");
+const salesTableTbody = salesTable.querySelector("tbody");
+
 
 const currentUrl = location.href;
 const url = new URL(currentUrl);
@@ -36,8 +39,11 @@ async function productProfile() {
             'Content-Type': 'application/json',
         },
     });
-    if(response.ok) {
-        let data = await response.json();
+    let data = await response.json();
+    if(data.length === 0) {
+        alert("현재 등록된 상품이 없습니다. 상품을 먼저 등록해주세요");
+        //to-do : move to create product page.
+    } else {
         data.forEach(element => {
             let productOption = document.createElement("option");
             productOption.setAttribute("value", `${element.name}`)
@@ -55,9 +61,72 @@ function handleDateInput() {
     dateInput.setAttribute("max", yesterdayValue);
 }
 
-// function saleRetrieve(event) {
-//     event.preventDefault();
-// }
+async function saleRetrieve(event) {
+    event.preventDefault();
+    let productName = productSelect.value;
+    let date = dateInput.value;
+    let response = await fetch(`${baseUrl}/api/v1/sales/?product=${productName}&date=${date}`, {
+        method : "GET",
+        credentials: "include",
+        headers : {
+            'Content-Type': 'application/json',
+        },
+    });
+    let data = await response.json();
+    if(data.length === 0) {
+        alert("선택 한 날짜에는 판매 데이터가 없습니다. 다른 날짜를 선택해주세요");
+    } else {
+        if(salesTableTbody.firstElementChild === null) {
+            data.forEach(element => {
+                let productNameTd = document.createElement("td");
+                let productCostTd = document.createElement("td");
+                let countTd = document.createElement("td");
+                let priceTd = document.createElement("td");
+                let deliveryPriceTd = document.createElement("td");
+                let payTimeTd = document.createElement("td");
+                let salesTableTr = document.createElement("tr");
+                productNameTd.innerText = `${element.product.name}`;
+                productCostTd.innerText = `${element.product.cost}`;
+                countTd.innerText = `${element.count}`;
+                priceTd.innerText = `${element.price}`;
+                deliveryPriceTd.innerText = `${element.delivery_price}`;
+                payTimeTd.innerText = `${element.pay_time}`;
+                salesTableTr.appendChild(productNameTd);
+                salesTableTr.appendChild(productCostTd);
+                salesTableTr.appendChild(countTd);
+                salesTableTr.appendChild(priceTd);
+                salesTableTr.appendChild(deliveryPriceTd);
+                salesTableTr.appendChild(payTimeTd);
+                salesTableTbody.appendChild(salesTableTr);
+            });
+        } else {
+            const tr = salesTableTbody.querySelector("tr");
+            tr.remove();
+            data.forEach(element => {
+                let productNameTd = document.createElement("td");
+                let productCostTd = document.createElement("td");
+                let countTd = document.createElement("td");
+                let priceTd = document.createElement("td");
+                let deliveryPriceTd = document.createElement("td");
+                let payTimeTd = document.createElement("td");
+                let salesTableTr = document.createElement("tr");
+                productNameTd.innerText = `${element.product.name}`;
+                productCostTd.innerText = `${element.product.cost}`;
+                countTd.innerText = `${element.count}`;
+                priceTd.innerText = `${element.price}`;
+                deliveryPriceTd.innerText = `${element.delivery_price}`;
+                payTimeTd.innerText = `${element.pay_time}`;
+                salesTableTr.appendChild(productNameTd);
+                salesTableTr.appendChild(productCostTd);
+                salesTableTr.appendChild(countTd);
+                salesTableTr.appendChild(priceTd);
+                salesTableTr.appendChild(deliveryPriceTd);
+                salesTableTr.appendChild(payTimeTd);
+                salesTableTbody.appendChild(salesTableTr);
+            });
+        }
+    }
+}
 
 
 // 모던 자바스크립트에서 찾은 내용
@@ -89,3 +158,4 @@ async function logOut() {
 userProfile();
 handleDateInput();
 logOutBtn.addEventListener("click", logOut);
+integrationForm.addEventListener("submit", saleRetrieve);
